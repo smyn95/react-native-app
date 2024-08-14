2fc5e9a06767a333df2c12f8c14b8aee79f496d3
smyn95 <tlsalduszz@gmail.com>
2024-08-14 14:10:33 +0900

    WebView 화면에 하단 메뉴 추가
    - 뒤로, 앞으로가 안된다면 비활성화

--- screens/BrowserScreen.tsx
+++ screens/BrowserScreen.tsx
@@ -1,8 +1,16 @@
 import React, { useMemo, useRef, useState } from 'react';
-import { Animated, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {
+  Animated,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import WebView from 'react-native-webview';
 import { RootStackParamList } from '../routes';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'browser'>;
 
@@ -25,11 +33,60 @@ const styles = StyleSheet.create({
     height: '100%',
     backgroundColor: 'green',
   },
+  navigator: {
+    backgroundColor: 'black',
+    flexDirection: 'row',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    justifyContent: 'space-between',
+  },
+  button: {
+    width: 30,
+    height: 30,
+    padding: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  naverIconOutline: {
+    borderWidth: 1,
+    borderColor: 'white',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  naverIconText: {
+    color: 'white',
+  },
 });
 
-const BrowserScreen = ({ route }: Props) => {
+const NavButton = ({
+  iconName,
+  disabled,
+  onPress,
+}: {
+  iconName: string;
+  disabled?: boolean;
+  onPress?: () => void;
+}) => {
+  const color = disabled ? 'gray' : 'white';
+
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      disabled={disabled}
+      onPress={onPress}
+    >
+      <MaterialCommunityIcons name={iconName} size={24} color={color} />
+    </TouchableOpacity>
+  );
+};
+
+const BrowserScreen = ({ route, navigation }: Props) => {
   const { initialUrl } = route.params;
   const [url, setUrl] = useState(initialUrl);
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
 
   const urlText = useMemo(
     () => url.replace('https://', '').split('/')[0],
@@ -37,6 +94,7 @@ const BrowserScreen = ({ route }: Props) => {
   );
 
   const progressAnim = useRef(new Animated.Value(0)).current;
+  const webViewRef = useRef<WebView>(null);
 
   return (
     <SafeAreaView style={styles.safearea}>
@@ -57,8 +115,11 @@ const BrowserScreen = ({ route }: Props) => {
         />
       </View>
       <WebView
+        ref={webViewRef}
         source={{ uri: initialUrl }}
         onNavigationStateChange={event => {
+          setCanGoBack(event.canGoBack);
+          setCanGoForward(event.canGoForward);
           setUrl(event.url);
         }}
         onLoadProgress={event => {
@@ -66,6 +127,38 @@ const BrowserScreen = ({ route }: Props) => {
         }}
         onLoadEnd={() => progressAnim.setValue(0)}
       />
+      <View style={styles.navigator}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            navigation.goBack();
+          }}
+        >
+          <View style={styles.naverIconOutline}>
+            <Text style={styles.naverIconText}>N</Text>
+          </View>
+        </TouchableOpacity>
+        <NavButton
+          iconName="arrow-left"
+          disabled={!canGoBack}
+          onPress={() => {
+            webViewRef.current?.goBack();
+          }}
+        />
+        <NavButton
+          iconName="arrow-right"
+          disabled={!canGoForward}
+          onPress={() => {
+            webViewRef.current?.goForward();
+          }}
+        />
+        <NavButton
+          iconName="refresh"
+          onPress={() => {
+            webViewRef.current?.reload();
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
