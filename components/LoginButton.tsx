2457facdb2328736dbde124ccc5a00e8bdbf3391
smyn95 <tlsalduszz@gmail.com>
2024-08-22 11:08:55 +0900

    사용자는 로그아웃을 할 수 있다
    - react-native-cookies 라이브러리 추가

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -1,22 +1,49 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useNavigation } from '@react-navigation/native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
 import { RootStackParamList, RouteNames } from '../routes';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import CookieManager from '@react-native-cookies/cookies';
+import { WebViewContext } from './WebViewProvider';
 
 type Props = NativeStackNavigationProp<RootStackParamList>;
 
 const LoginButton = () => {
+  const context = useContext(WebViewContext);
   const navigation = useNavigation<Props>();
+  const isFocused = useIsFocused();
 
-  const isLoggedIn = false;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const iconName = isLoggedIn ? 'logout' : 'login';
+
+  useEffect(() => {
+    if (isFocused) {
+      CookieManager.get('https://.naver.com', true).then(cookie => {
+        console.log(cookie);
+        if (cookie.NID_SES) {
+          setIsLoggedIn(true);
+        } else {
+          setIsLoggedIn(false);
+        }
+      });
+    }
+  }, [isFocused]);
 
   const onPressLogin = useCallback(() => {
     navigation.navigate(RouteNames.LOGIN);
   }, [navigation]);
-  const onPressLogout = useCallback(() => {}, []);
+
+  const onPressLogout = useCallback(async () => {
+    await CookieManager.clearAll(true);
+    setIsLoggedIn(false);
+
+    if (context?.webViewRefs.current != null) {
+      context.webViewRefs.current.forEach(webView => {
+        webView.reload();
+      });
+    }
+  }, [context]);
 
   return (
     <TouchableOpacity onPress={isLoggedIn ? onPressLogout : onPressLogin}>
